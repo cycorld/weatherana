a83c43d05e1f994e00d35116a0ac7cfc2ec89dab
Charles Choi <charles@Charles-ui-MacBook-Air.local>
2013-04-21 06:41:12 +0900

    sapjil

--- gen/net/likelion/weatheralarm/R.java
+++ gen/net/likelion/weatheralarm/R.java
@@ -26,16 +26,18 @@ public final class R {
         public static final int splash_image=0x7f020002;
     }
     public static final class id {
-        public static final int action_settings=0x7f090012;
+        public static final int action_settings=0x7f090015;
         public static final int alarmtoggle=0x7f090009;
         public static final int alarmtoggle_text=0x7f09000a;
+        public static final int fullscreen_custom_content=0x7f090012;
         public static final int intro_txt=0x7f090006;
-        public static final int pause=0x7f09000e;
+        public static final int main_content=0x7f090013;
+        public static final int pause=0x7f090011;
         public static final int play_btn=0x7f090008;
         public static final int play_text=0x7f090007;
         public static final int set_time=0x7f09000c;
-        public static final int splash_image=0x7f090010;
-        public static final int stop=0x7f09000f;
+        public static final int splash_image=0x7f09000f;
+        public static final int stop=0x7f09000e;
         public static final int textView1=0x7f090000;
         public static final int textView2=0x7f090001;
         public static final int textView3=0x7f090002;
@@ -43,8 +45,9 @@ public final class R {
         public static final int textView5=0x7f090005;
         public static final int textView6=0x7f090004;
         public static final int timePicker=0x7f09000d;
-        public static final int timer_text=0x7f090011;
+        public static final int timer_text=0x7f090010;
         public static final int update_btn=0x7f09000b;
+        public static final int webView=0x7f090014;
     }
     public static final class layout {
         public static final int activity_download=0x7f030000;
